b30fdf618b5ec3edb598c270982de1e8a10dd424
Esolano <daveysolano124@gmail.com>
2025-09-22 21:05:56 -0500

    Esolano -WIesolano feat: se termina y cierra el proyecto

--- ProySobrecarga/src/com/esolano/poosobrecarga/ClsCalculadora.java
+++ ProySobrecarga/src/com/esolano/poosobrecarga/ClsCalculadora.java
@@ -4,19 +4,46 @@ public class ClsCalculadora {
     //---> se puede colocar el mismo nombre a varios metodos ya que no
     //-- tienen los mismos tipos de datos
 
-    public int sumar(int a, int b){
+
+    public ClsCalculadora() {
+    }
+
+    public static int sumar(int... argumentos){
+         int total = 0;
+         for(int i: argumentos){
+             total+= i;
+         }
+         return  total;
+     }
+
+    public static float sumar(float a ,int... argumentosflt){
+        float total = 0;
+        for(int i: argumentosflt){
+            total+= i;
+        }
+        return  total;
+    }
+    public static double sumar(double... vargumentos){
+        double total = 0.0;
+        for(double d : vargumentos){
+            total += d;
+        }
+        return   total;
+    }
+
+    public static int sumar(int a, int b){
         return a+b;
     }
 
-    public float sumar(float a, int b){
+    public static float sumar(float a, int b){
         return a + b;
     }
 
-    public float sumar(float c, float d){
+    public static float sumar(float c, float d){
         return c + d;
     }
 
-    public int sumar (String e, String f){
+    public static int sumar (String e, String f){
         int resultado;
         try {
             resultado = Integer.parseInt(e) + Integer.parseInt(f);
--- ProySobrecarga/src/com/esolano/poosobrecarga/ClsEjemploSobreCarga.java
+++ ProySobrecarga/src/com/esolano/poosobrecarga/ClsEjemploSobreCarga.java
@@ -1,13 +1,17 @@
 package com.esolano.poosobrecarga;
-
+import  static  com.esolano.poosobrecarga.ClsCalculadora.*; // asi llamo a todos los metodos de la clase y no es necesario usar el constructor
 public class ClsEjemploSobreCarga {
     public static void main(String[] args) {
 
-        ClsCalculadora cal = new ClsCalculadora();
 
-        System.out.println("sumar int : " + cal.sumar(10 , 2));
-        System.out.println("sumar float : " + cal.sumar(10f, 2f));
-        System.out.println("sumar String : " + cal.sumar("15", "45"));
+        System.out.println("sumar int : " + ClsCalculadora.sumar(10 , 2));
+        System.out.println("sumar float : " + ClsCalculadora.sumar(10f, 2f));
+        System.out.println("sumar String : " + ClsCalculadora.sumar("15", "45"));
+        System.out.println("Sumar 4 int:  " + ClsCalculadora.sumar(10,5,2,3));
+        System.out.println("Sumar 4 int:  " + ClsCalculadora.sumar(10.4,5,2,3));
+        System.out.println("Sumar 4 float:  " + sumar(10.4,5.2,2.3,3.8));
+
+
     }
 
 }
