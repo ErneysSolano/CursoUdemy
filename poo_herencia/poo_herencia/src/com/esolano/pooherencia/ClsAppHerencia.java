986282bbec08a349fc41f0ac2f3efb86d70266e5
Esolano <daveysolano124@gmail.com>
2025-09-22 21:44:56 -0500

    Esolano-WIesolano feat: se realiza ejemplo de herencia padre-hijo

---
+++ poo_herencia/poo_herencia/src/com/esolano/pooherencia/ClsAppHerencia.java
@@ -0,0 +1,19 @@
+package com.esolano.pooherencia;
+
+public class ClsAppHerencia {
+    public static void main(String[] args) {
+
+        ClsAlumno alumno = new ClsAlumno();
+        alumno.setNombre("Erneys");
+        alumno.setApellido("Solano");
+
+        ClsProfesor profe = new ClsProfesor();
+        profe.setNombre("Juan");
+        profe.setApellido("Quintero");
+        profe.setAsignatura("Sociales");
+
+
+
+
+    }
+}
---
+++ poo_herencia/poo_herencia/src/com/esolano/pooherencia/ClsProfesor.java
@@ -0,0 +1,13 @@
+package com.esolano.pooherencia;
+
+public class ClsProfesor extends  ClsPersona {
+    private String asignatura;
+
+    public String getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(String asignatura) {
+        this.asignatura = asignatura;
+    }
+}
