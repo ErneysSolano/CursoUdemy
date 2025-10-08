2435caed1c7b0e9002a7f526b345e991d3f337f6
Erneys Solano <esolano@cobiscorp.int>
2025-10-08 16:58:56 -0500

    Esolano-RAMesolano op: Se realiza logica

--- ProyBase/ProyBase/src/com/esolano/app/ClsApp.java
+++ ProyBase/ProyBase/src/com/esolano/app/ClsApp.java
@@ -1,11 +1,30 @@
 package com.esolano.app;
 
 import com.esolano.bllt.ClsBilletes;
+import com.esolano.mnd.ClsMnds;
+
+
+import java.util.Scanner;
 
 public class ClsApp {
     public static void main(String[] args) {
 
-        ClsBilletes bllts = new ClsBilletes();
-        System.out.println("valor " + bllts.getFajBillT2());
+        int acumMnds = 0;
+
+      Scanner sc = new Scanner(System.in);
+        ClsMnds  mnds = new ClsMnds();
+        System.out.println("Ingrese valor mnd50");
+        mnds.setMnd50(sc.nextInt());
+        while(mnds.getMnd50() != 1000 && mnds.getMnd50() != 2000) {
+            System.out.println("Valor mnd50 invalido");
+            mnds.setMnd50(sc.nextInt());
+        }
+        System.out.println("Ingrese valor mnd100");
+        mnds.setMnd100(sc.nextInt());
+
+
+        acumMnds += mnds.getMnd100() + mnds.getMnd50();
+
+        System.out.println("valor acum " +  acumMnds);
     }
 }
--- ProyBase/ProyBase/src/com/esolano/mnd/ClsMnds.java
+++ ProyBase/ProyBase/src/com/esolano/mnd/ClsMnds.java
@@ -1,13 +1,64 @@
 package com.esolano.mnd;
 
 public class ClsMnds {
-    private Integer mnd50 = 2000;
-    private Integer mnd100 = 4000;
-    private Integer mnd2000 = 5000;
-    private Integer mnd500 = 10000;
-    private Integer mnd1000 = 10000;
-
-    
+    private Integer mnd50;
+    private Integer mnd100;
+    private Integer mnd200;
+    private Integer mnd500;
+    private Integer mnd1000;
 
 
+    public ClsMnds(Integer mnd50, Integer mnd100, Integer mnd200, Integer mnd500, Integer mnd1000) {
+        this.mnd50 = mnd50;
+        this.mnd100 = mnd100;
+        this.mnd200 = mnd200;
+        this.mnd500 = mnd500;
+        this.mnd1000 = mnd1000;
+    }
+
+    public ClsMnds() {
+    }
+
+    //---- getter ------
+
+
+    public Integer getMnd50() {
+        return mnd50;
+    }
+
+    public void setMnd50(Integer mnd50) {
+        this.mnd50 = mnd50;
+    }
+
+    public Integer getMnd1000() {
+        return mnd1000;
+    }
+
+    public void setMnd1000(Integer mnd1000) {
+        this.mnd1000 = mnd1000;
+    }
+
+    public Integer getMnd500() {
+        return mnd500;
+    }
+
+    public void setMnd500(Integer mnd500) {
+        this.mnd500 = mnd500;
+    }
+
+    public Integer getMnd200() {
+        return mnd200;
+    }
+
+    public void setMnd200(Integer mnd200) {
+        this.mnd200 = mnd200;
+    }
+
+    public Integer getMnd100() {
+        return mnd100;
+    }
+
+    public void setMnd100(Integer mnd100) {
+        this.mnd100 = mnd100;
+    }
 }
